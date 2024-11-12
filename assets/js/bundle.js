a16e88fbd8785345b79a3097845fea3c7b61d1c8
Clay Ewing <c.ewing@miami.edu>
2024-11-12 11:26:00 -0600

    update for student work

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -29,12 +29,12 @@ Only on mobile devices
 */
 
 const menu = document.getElementById('menu');
-const searchBox = document.getElementById('search');
+//const searchBox = document.getElementById('search');
 const menuToggle = document.getElementById('menu-toggle');
 
 menuToggle.addEventListener('click', () => {
   menu.classList.toggle('hidden');
-  searchBox.classList.toggle('hidden');
+//  searchBox.classList.toggle('hidden');
 }, false);
 
 /*
